819aa6ffa52a080a7169e3a5432b1ac460f1b35b
Layla Janaína Hissa Borges <99913142+LaylaJHB@users.noreply.github.com>
2022-04-22 19:39:42 -0300

    exercício da aula de funções

---
+++ modulo1/funcoes/index.js
@@ -0,0 +1,43 @@
+//----*EXERCÍCIO DE INTERPRETAÇÃO DE CÓDIGO* ------
+
+/*
+1 a) 
+
+Serão impressos os valores das variáveis multiplicados por 5, ou seja: 10 e 50 
+
+1b)  
+
+Não será impresso nada
+
+
+2) Falta
+
+*/
+
+
+//----*EXERCÍCIO DE ESCRITA DE CÓDIGO* ------
+/* 
+1) a) A função não deve receber nenhum parâmetro e deve imprimir uma mensagem falando algumas informações sobre você, como: 
+
+"Eu sou Caio, tenho 23 anos, moro em São Paulo e sou estudante."
+
+Troque o nome, idade, cidade e se é estudante ou não por informações sobre você. Lembrando que a função não possui entradas, apenas imprime essa mensagem. 
+
+
+function mensagemPerfil() {
+console.log("Eu sou Layla, tenho 39 anos, moro em São Paulo e sou estudante.")
+}
+
+mensagemPerfil()
+
+b)  Agora escreva uma função que receba 4 parâmetros que correspondem às informações de uma pessoa: o nome (string), a idade (number), a cidade (string) e uma profissão (string). Ela deve retornar uma string que unifique todas as informações da pessoa em uma só mensagem com o template:
+
+Eu sou [NOME], tenho [IDADE] anos, moro em [ENDEREÇO] e sou [PROFISSÃO].
+
+
+Corrigir -----
+function infoPessoa(nome, idade, cidade, profissao)
+
+console.log(`Olá ${nome} , ${idade}, ${cidade}, ${profissao}`) 
+*/
+
