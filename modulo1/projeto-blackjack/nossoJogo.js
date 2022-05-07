f23d0ccd51178f466f3ab782b7a50727fde7b135
Layla Janaína Hissa Borges <99913142+LaylaJHB@users.noreply.github.com>
2022-05-06 22:55:46 -0300

    Projeto BlackJack

--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -12,42 +12,51 @@
  */
 
 
-    console.log('Boas vindas ao jogo BlackJack!')
 
-    const carta = comprarCarta()
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
     
-    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ 
+ */
 
-    if (confirm('Quer iniciar uma nova rodada?')) {
-      comprarCarta(confirm)
-      console.log(carta.texto)
-      console.log(carta.valor)
-      console.log('Usuário - cartas:', carta.texto, 'pontuação - ', carta.valor )
-      comprarCarta()
-      console.log(carta.texto)
-      console.log(carta.valor)
-      console.log('Usuário - cartas:', carta.texto, 'pontuação - ', carta.valor )
-      
-      
-    } else { 
-       console.log('O jogo acabou')
-    }
 
-    
-      comprarCarta(confirm)
-      console.log(carta.texto)
-      console.log(carta.valor)
-      console.log('Computador - cartas:', carta.texto, 'pontuação - ', carta.valor )
+// ---------- PROJETO DE FIXAÇÃO: BLACKJACK ---------------------
 
+console.log(`"Boas vindas ao jogo do Blackjack!"`)
+
+const cartaUsuario1 = comprarCarta()
+const cartaUsuario2 = comprarCarta()
+const cartaComputador1 = comprarCarta()
+const cartaComputador2 = comprarCarta()
+if(confirm("Quer iniciar uma nova rodada?")) {
+   //console.log('Carta 1 do usuário: ', cartaUsuario1.texto, cartaUsuario1.valor)
+   //console.log('Carta 2 do usuário: ', cartaUsuario2.texto,cartaUsuario2.valor)
+  // console.log('Carta 1 do computador: ', cartaComputador1.texto,cartaComputador1.valor)
+   //console.log('Carta 2 do computador: ', cartaComputador2.texto,cartaComputador2.valor)
    
-   /* console.log('Usuário - cartas:', carta , pontuacao)
-    
-    if (confirm('Quer comprar uma segunda carta?')) {
-      comprarCarta(confirm)
-      console.log(carta.texto)
-      console.log(carta.valor)
-    } else { 
-       console.log('O jogo acabou')
+   let resultadoUsuario = cartaUsuario1.valor + cartaUsuario2.valor 
+   //console.log(resultadoUsuario)
 
-    }*/
-   
+   let resultadoComputador = cartaComputador1.valor + cartaComputador2.valor
+   //console.log(resultadoComputador)
+
+   console.log(`"Usuário - cartas: ${cartaUsuario1.texto} ${cartaUsuario2.texto} - pontuação ${resultadoUsuario}"` )
+
+   console.log(`"Computador - cartas: ${cartaComputador1.texto} ${cartaComputador2.texto} - pontuação ${resultadoComputador}"` )
+
+   if (resultadoUsuario === resultadoComputador){
+         console.log(`"Empate!"`)
+   } else if (resultadoUsuario > resultadoComputador){
+         console.log(`"O usuário ganhou!"`)
+   } else if (resultadoUsuario < resultadoComputador){
+         console.log(`"O computador ganhou!"`)
+   } 
+
+} else {
+   console.log("O jogo acabou")
+}
