38b97345497d490493dbdfcdc6c0e3e09127f42b
Layla Janaína Hissa Borges <99913142+LaylaJHB@users.noreply.github.com>
2022-07-28 18:47:23 -0300

    Exercício Integração de API

---
+++ modulo3/apis-labenusers/apis-labenusers/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import DadosUsuarios from './Components/DadosUsuarios';
+
+function App() {
+  return (
+    <div className="App">
+      <DadosUsuarios></DadosUsuarios>
+    </div>
+  );
+}
+
+export default App;
