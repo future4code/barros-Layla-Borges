603ba1244be82ed7d111adbf98a4078b08521740
Layla Janaína Hissa Borges <99913142+LaylaJHB@users.noreply.github.com>
2022-08-16 21:37:40 -0300

    Adição do form de Login Email e Senha

--- modulo4/labex/src/pages/LoginPage.js
+++ modulo4/labex/src/pages/LoginPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-
+import axios from "axios"
+import {useState} from "react"
 
 function Login() {
 
@@ -10,10 +11,48 @@ function Login() {
         navigate(-1)
       }
 
+      const [email, setEmail] = useState("")
+      const [senha, setSenha] = useState("")
+  
+      const EmailMudanca = (event) => {
+          setEmail(event.target.value)
+      }
+      const SenhaMudanca = (event) => {
+          setSenha(event.target.value)
+      }
+  
+      const body = {
+          "email": email,
+          "password": senha
+      }
+  
+      const fazerLogin = () => {
+          //axios.post(url, body, headers) - Vou trabalhar esse ponto, não esquecer!
+          console.log(body)
+      }
+
     return (
         <section>
             <h1>Login</h1>
+
+
+            <div>
+            <input
+                placeholder="E-mail"
+                value={email}
+                onChange={EmailMudanca}
+                type="email"
+            />
+            
+            <input 
+                placeholder="Senha"
+                value={senha}
+                onChange={SenhaMudanca}
+                type="password"
+            />
+            <button onClick={fazerLogin}>Login</button>
             <button onClick={goToLastPage}>Voltar</button>
+        </div>
         </section>
         
     );
