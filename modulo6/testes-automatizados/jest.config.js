0017fa2d357832683c06a3fcc719eb7b9dcae80e
Layla Janaína Hissa Borges <99913142+LaylaJHB@users.noreply.github.com>
2023-02-28 13:23:56 -0300

    (i) Atualização do arquivo tsconfig.json retirando o src do direcionamento do root rootDir, (ii) Adição do arquivo .gitignore

--- modulo6/testes-automatizados/jest.config.js
+++ modulo6/testes-automatizados/jest.config.js
@@ -1,8 +1,7 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */ 
+
 module.exports = {
-  roots: ["<rootDir>/tests"],
-  transform: {
-    "^.+\\.tsx?$": "ts-jest",
-  },
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  roots: ['<rootDir>/tests'],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
 };
